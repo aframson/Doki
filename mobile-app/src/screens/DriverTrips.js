dce67cc1b0dcdd7fbe9af5eadafa508b20a51a4f
aframson <obiriaframson@gmail.com>
2021-10-31 21:18:08 +0000

    last update on doki part 3

--- mobile-app/src/screens/DriverTrips.js
+++ mobile-app/src/screens/DriverTrips.js
@@ -157,9 +157,9 @@ export default function DriverTrips(props) {
                                 <View style={styles.dateView}>
                                     <Text style={styles.listDate}>{new Date(item.tripdate).toLocaleString(dateStyle)}</Text>
                                 </View>
-                                <View style={styles.rateViewStyle}>
+                                {/* <View style={styles.rateViewStyle}>
                                     <Text style={styles.rateViewTextStyle}>{settings.symbol}{item ? item.estimate > 0 ? parseFloat(item.estimate).toFixed(2) : 0 : null}</Text>
-                                </View>
+                                </View> */}
                                 <View style={styles.estimateView}>
                                     <Text style={styles.listEstimate}>{item.estimateDistance? parseFloat(item.estimateDistance).toFixed(2): 0} {settings.convert_to_mile? language.mile : language.km}</Text>
                                     <Text style={styles.listEstimate}>{item.estimateTime? parseFloat(item.estimateTime/60).toFixed(0): 0} {language.mins}</Text>
